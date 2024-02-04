ce44a168165f6ab0e739a77488ac4cfe90b8d63b
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-02-04 20:04:12 +0100

    Ejercicio_1

--- 1/exercise/exercise.js
+++ 1/exercise/exercise.js
@@ -4,14 +4,32 @@
 // developer => 1500
 // default => 1000
 function calculateSalary(role) {
-  // ...
+  let salary = 0;
+  switch (role) {
+    case "ceo":
+      salary = 2200;
+      break;
+    case "manager":
+      salary = 1800;
+      break;
+    case "cto":
+      salary = 1800;
+      break;
+    case "developer":
+      salary = 1500;
+      break;
+    default:
+      salary = 1000;
+      break;
+  }
+  return salary;
 }
 
-const ceoSalary = calculateSalary('ceo');
-const managerSalary = calculateSalary('manager');
-const ctoSalary = calculateSalary('cto');
-const developerSalary = calculateSalary('developer');
-const otherSalary = calculateSalary('other');
+const ceoSalary = calculateSalary("ceo");
+const managerSalary = calculateSalary("manager");
+const ctoSalary = calculateSalary("cto");
+const developerSalary = calculateSalary("developer");
+const otherSalary = calculateSalary("other");
 
 console.log(ceoSalary);
 console.log(managerSalary);
