8a34044fca8aae91bac2feae4caaf07b9f7e0ac2
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-02-06 08:49:03 +0100

    Ejercicio 19

--- 19/exercise/exercise.js
+++ 19/exercise/exercise.js
@@ -1,5 +1,41 @@
 class Person {
-  // ...
+  firstName;
+  lastName;
+  age;
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  
+  set firstName(newVal) {
+    this.firstName = newVal;
+  }
+  
+  get firstName() {
+    return this.firstName;
+  }
+  
+  set lastName(newVal) {
+    this.lastName = newVal;
+  }
+  
+  get lastName() {
+    return this.lastName;
+  }
+  
+  set age(newVal) {
+    this.age = newVal;
+  }
+  
+  get age() {
+    return this.age;
+  }
+
+  get fullName() {
+    return this.firstName + " " + this.lastName + " " + this.age;
+  }
+
 }
 
 const person = new Person('Mario', 'Rossi', 25);
