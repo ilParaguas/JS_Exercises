7b7dd092ef775a20dbb5509a7b95e00607cdbbec
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-02-06 22:09:09 +0100

    Ejercicio 37

--- 30/exercise/README.md
+++
@@ -1,3 +0,0 @@
-# JSON methods
-
-Create the `fromJson` method that takes in a json as parameter and returns an object of type `Person`.
--- 37/exercise/exercise.js
+++ 37/exercise/exercise.js
@@ -1,1 +1,13 @@
 const number = 15;
+
+let promise = new Promise((resolve, reject) => {
+  if (number > 10) {
+    resolve(number);
+  } else {
+    reject(number);
+  }
+});
+
+promise
+  .then((num) => console.log(num))
+  .catch((err) => console.error(err));
