889087d79c61a7777e5a3348c2f9a3ccda0eb795
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-02-06 01:23:02 +0100

    Ejercicio_5

--- 5/exercise/exercise/exercise.js
+++ 5/exercise/exercise/exercise.js
@@ -7,6 +7,12 @@ const person1 = {
 const person2 = person1;
 
 // Modify the property `firstName` of the `person2` in "Simon".
+person2.firstName = "Simon";
+/*
+When you modify the object 2, the object 1 changes too because
+when we declared object 2, it was declared as a reference to the
+first one,so it's not an independent object itself.
+*/
 
 console.log(person1);
 console.log(person2);
