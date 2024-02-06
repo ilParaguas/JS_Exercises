9cdd0cf8e3565fe0fd9e498f7570c1a0450d6f0d
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-02-06 21:29:21 +0100

    Ejercicio 34

--- 34/exercise/exercise.js
+++ 34/exercise/exercise.js
@@ -1,1 +1,10 @@
+function printAsyncName(callback, name) {
+  setTimeout(() => {
+    callback();
+  }, 1000);
+  setTimeout(() => {
+    console.log(name);
+  }, 2000);
+}
 
+printAsyncName(() => console.log("Hello"), "Javi");
