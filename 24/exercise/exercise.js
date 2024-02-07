2ae91ab06dba5f2edf9f26fb695cc05c1195acc8
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-02-07 08:57:03 +0100

    Ejercicio 39

--- 24/exercise/exercise.js
+++
@@ -1,9 +0,0 @@
-const person = {
-  id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-};
-
-const { id, name, surname, old } = person;
-console.log(id, name, surname, old);
--- 39/exercise/exercise.js
+++ 39/exercise/exercise.js
@@ -1,1 +1,29 @@
 
+const isLogged = true;
+
+function promise() {
+  return new Promise((resolve, reject) => {
+    let rand = Math.random();
+    if (isLogged == true) {
+      resolve(rand);
+    } else {
+      reject(new Error("Error"));
+    }
+  });
+}
+
+function promise2(num) {
+  return new Promise((resolve, reject) => {
+    if (num > 0.5) {
+      resolve({ name: "John", age: 24 });
+    } else {
+      reject(new Error("Error 2"));
+    }
+  });
+}
+
+promise()
+  .then((val) => promise2(val))
+  .then((name) => console.log(name))
+  .catch((err) => console.error(err))
+  .finally(() => console.log("Finally!"));
